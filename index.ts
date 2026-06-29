2ac6a3cae243255f3dba754af1468ace6377bd57
Aryan Sharma <aryansharma24106@gmail.com>
2026-06-29 09:16:10 +0530

    feat: scaffold working prototype of url shortner

---
+++ index.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import crypto from 'crypto';
+
+const app = express(); 
+const PORT = 3000;
+
+const urlDB: Record<string, string> = {};
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello from my server");
+})
+
+app.post('/url', (req, res) => {
+
+    const {url} = req.body;
+    if (url) {
+        const token = crypto.randomBytes(4).toString('hex');
+        urlDB[token] = url;
+        res.json({ "Shortened URL" : `http://localhost:3000/${token}` });
+    }   
+    else{
+        res.end("No url submitted, Try again!");
+    } 
+})
+
+app.get("/:shortId", (req, res)=>{
+    const { shortId } = req.params;
+    const redirect = urlDB[shortId];
+    res.redirect(redirect);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
